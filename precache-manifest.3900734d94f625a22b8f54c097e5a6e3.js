dd2c136235e4c842e737a858d64437efcfc07859
Станислав Гиль <stanislavgil@MacBook-Air-Stanislav.local>
2019-11-23 16:23:36 +0200

    Updates

---
+++ precache-manifest.3900734d94f625a22b8f54c097e5a6e3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a87e47d455d631eefc31011af52ade4",
+    "url": "/goit-react-hw-02-bank-account/index.html"
+  },
+  {
+    "revision": "4e1fbe7bc4ae6898f6f1",
+    "url": "/goit-react-hw-02-bank-account/static/css/2.5dbdccff.chunk.css"
+  },
+  {
+    "revision": "ff53c2ad12e455f0d555",
+    "url": "/goit-react-hw-02-bank-account/static/css/main.eb636f57.chunk.css"
+  },
+  {
+    "revision": "4e1fbe7bc4ae6898f6f1",
+    "url": "/goit-react-hw-02-bank-account/static/js/2.fd695f14.chunk.js"
+  },
+  {
+    "revision": "ff53c2ad12e455f0d555",
+    "url": "/goit-react-hw-02-bank-account/static/js/main.4a637db0.chunk.js"
+  },
+  {
+    "revision": "59b69276754240abb686",
+    "url": "/goit-react-hw-02-bank-account/static/js/runtime-main.611f8d42.js"
+  }
+]);
